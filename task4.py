7f71e9a0bb582e15a451aab8566d21bb249cf9ef
DimonYarkin <dmitryyarkin@yandex.ru>
2020-09-24 22:31:52 +0300

    Задачи выполнены

--- task4.py
+++ task4.py
@@ -29,7 +29,7 @@ class TownCar(Car):
         print(f' Текущая скорость городской машины {self.name} составляем {self.speed}')
 
         if self.speed > 60:
-            return f'Машина {self.name} привысела скорость органичение 40 км/ч'
+            return f'Машина {self.name} привысела скорость органичение 60 км/ч'
         else:
             return f'Машина {self.name} едет с нормальной скоростью'
 
@@ -47,7 +47,7 @@ class WorkCar(Car):
         print(f'Текущая скорость машины {self.name} составляет {self.speed}')
 
         if self.speed > 40:
-            return f'Машина {self.name} привысила скорость ограниечение 60 км/ч'
+            return f'Машина {self.name} привысила скорость ограниечение 40 км/ч'
 
 
 class PoliceCar(Car):
@@ -56,14 +56,23 @@ class PoliceCar(Car):
 
     def police(self):
         if self.is_police:
-            return f'Машина {self.name} полиции'
+            return f'Машина {self.name} принадлежит полиции'
         else:
             return f'Машина {self.name} не принадлежит полиции'
 
 
 audi = SportCar(100, 'Red', 'Audi', False)
-Mazda = TownCar(30, 'White', 'Mazda', False)
+mazda = TownCar(70, 'White', 'Mazda', False)
 lada = WorkCar(70, 'Rose', 'Lada', True)
 ford = PoliceCar(110, 'Blue', 'Ford', True)
-print(f"Путь спорт кара {audi.name} цвета {audi.color} ")
-print(f"{audi.go()} : {audi.show_speed()} \n{audi.turn_left()} \n{audi.turn_right()} \n{audi.stop()}")
+print(f"\033[32mПуть спорт кара {audi.name} цвета {audi.color}\n")
+print(f"\033[33m{audi.go()} : {audi.show_speed()} \n{audi.turn_left()} \n{audi.turn_right()} \n{audi.stop()}\n")
+
+print(f"\033[32mПуть городской машины {mazda.name} цвета {mazda.color}\n")
+print(f"\033[33m{mazda.go()} : {mazda.show_speed()} \n{mazda.turn_left()} \n{mazda.turn_right()} \n{mazda.stop()}\n")
+
+print(f"\033[32m Путь рабочей машины {lada.name} цвета {lada.color}\n")
+print(f"\033[33m{lada.go()} : {lada.show_speed()} \n{lada.turn_left()} \n{lada.turn_right()} \n{lada.stop()}\n")
+
+print(f"\033[32mПуть машины {ford.name} цвета {ford.color} {ford.police()}\n")
+print(f"\033[33m{ford.go()} : {ford.show_speed()} \n{ford.turn_left()} \n{ford.turn_right()} \n{ford.stop()}\n")
